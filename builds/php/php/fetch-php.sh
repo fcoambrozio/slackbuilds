4610828291dd3ea82abcc58915cc5130e2738a62
Francisco Ambrozio <git@franciscoambrozio.com>
2017-11-24 14:04:43 -0200

    php-7.1.12

--- builds/php/php/fetch-php.sh
+++ builds/php/php/fetch-php.sh
@@ -1,1 +1,8 @@
-lftpget http://us.php.net/distributions/php-7.1.11.tar.xz
+#!/bin/sh
+
+if [ -z $VERSION ]; then
+    echo 'Error: missing variable $VERSION'
+    exit 1
+fi
+
+lftpget http://us.php.net/distributions/php-$VERSION.tar.xz
