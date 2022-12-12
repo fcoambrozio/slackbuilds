34ec1226132d4fae9d2ba91937f36edba6ac2bda
Francisco Ambrozio <dev@franciscoambrozio.com>
2022-12-12 10:49:43 -0300

    20221212 update: vscode-bin

---
+++ vscode-bin/getsrc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# get source for vscode-bin
+#
+
+DOWNLOAD="https://az764295.vo.msecnd.net/stable/5235c6bb189b60b01b1f49062f4ffa42384f8c91/code-stable-x64-1670260359.tar.gz"
+
+wget -c $DOWNLOAD
