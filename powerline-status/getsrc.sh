9a8d498f44953321aad1bf7e916ecb0a4bd4d850
Francisco Ambrozio <dev@franciscoambrozio.com>
2022-12-12 10:08:41 -0300

    20221212 update: powerline-status

---
+++ powerline-status/getsrc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# get source for powerline-status
+#
+
+VERSION="2.8.3"
+DOWNLOAD="https://github.com/powerline/powerline/archive/$VERSION/powerline-$VERSION.tar.gz"
+
+wget -c $DOWNLOAD
