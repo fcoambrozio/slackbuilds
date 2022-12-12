9a0f4ec7b9d72d88a485ca2b824b082f123f3720
Francisco Ambrozio <dev@franciscoambrozio.com>
2022-12-12 10:22:44 -0300

    20221212 update: google-go-lang

---
+++ google-go-lang/getsrc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# get source for google-go-lang
+#
+
+VERSION="1.19.4"
+GOBOOTSTRAPSRC="https://dl.google.com/go/go1.4-bootstrap-20171003.tar.gz"
+DOWNLOAD="https://go.dev/dl/go$VERSION.src.tar.gz"
+
+case "$( uname -m )" in
+    i?86) ARCH=i586 ;;
+    arm*) ARCH=arm ;;
+       *) ARCH=$( uname -m ) ;;
+esac
+
+if [ "$ARCH" = "i586" ]; then
+    wget -c $GOBOOTSTRAPSRC
+fi
+
+wget -c $DOWNLOAD
