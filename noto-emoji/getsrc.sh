fdecdf9a632a1f6125d346bea1966a2f5d2f14a3
Francisco Ambrozio <dev@franciscoambrozio.com>
2022-12-12 16:27:51 -0300

    20221212 update: noto-emoji

---
+++ noto-emoji/getsrc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# get source for noto-emoji
+#
+
+VERSION="2.038"
+DOWNLOAD="https://github.com/googlei18n/noto-emoji/archive/v$VERSION/noto-emoji-$VERSION.tar.gz"
+
+wget -c $DOWNLOAD
