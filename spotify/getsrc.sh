c27ec0b939ad3b54f337a11e3f530e12c29becd3
Francisco Ambrozio <dev@franciscoambrozio.com>
2022-12-12 10:44:01 -0300

    20221212 update: spotify

---
+++ spotify/getsrc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# get source for spotify
+#
+
+case "$( uname -m )" in
+    i?86) ARCH=i586 ;;
+    arm*) ARCH=arm ;;
+       *) ARCH=$( uname -m ) ;;
+esac
+
+
+if [ "$ARCH" = "x86_64" ]; then
+    DOWNLOAD="http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.1.84.716.gc5f8b819-2_amd64.deb"
+else
+    DOWNLOAD="http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.0.72.117.g6bd7cc73-35_i386.deb"
+fi
+
+wget -c $DOWNLOAD
