dc9ed205577e7db44320bd6ff6b8ce153415e5bb
Francisco Ambrozio <dev@franciscoambrozio.com>
2023-02-03 15:48:34 -0300

    wolfssl: Updated for version 5.5.4

--- qemu/wolfssl/getsrc.sh
+++ qemu/wolfssl/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for wolfssl
 #
 
-VERSION="5.5.3"
+VERSION="5.5.4"
 DOWNLOAD="https://github.com/wolfssl/wolfssl/archive/refs/tags/v$VERSION-stable.tar.gz"
 
 wget -c $DOWNLOAD -O wolfssl-$VERSION-stable.tar.gz
