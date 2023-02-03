3c847fca63715a776bf893a51e22ade2a092e002
Francisco Ambrozio <dev@franciscoambrozio.com>
2023-02-03 15:35:56 -0300

    gst-plugins-ugly: Updated for version 1.22.0

--- qemu/gst-plugins-ugly/getsrc.sh
+++ qemu/gst-plugins-ugly/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for gst-plugins-ugly - https://gstreamer.freedesktop.org/
 #
 
-VERSION="1.20.5"
+VERSION="1.22.0"
 DOWNLOAD="https://gstreamer.freedesktop.org/src/gst-plugins-ugly/gst-plugins-ugly-$VERSION.tar.xz"
 
 wget -4 -c $DOWNLOAD
