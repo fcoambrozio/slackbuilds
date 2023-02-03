8d045b77a727b6e412c6a1c5accf1a7a09cacffc
Francisco Ambrozio <dev@franciscoambrozio.com>
2023-02-03 15:45:46 -0300

    virglrenderer: Updated for version 0.10.4

--- qemu/virglrenderer/getsrc.sh
+++ qemu/virglrenderer/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for virglrenderer - https://docs.mesa3d.org/drivers/virgl
 #
 
-VERSION="0.10.3"
+VERSION="0.10.4"
 DOWNLOAD="https://gitlab.freedesktop.org/virgl/virglrenderer/-/archive/$VERSION/virglrenderer-$VERSION.tar.bz2"
 
 wget -c $DOWNLOAD
