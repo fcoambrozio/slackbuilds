93d95b7f3704466817c427d8bd070fd128123221
Francisco Ambrozio <dev@franciscoambrozio.com>
2023-02-03 11:25:07 -0300

    docker-proxy: Updated for version 20230118_05b93e0

--- docker/docker-proxy/getsrc.sh
+++ docker/docker-proxy/getsrc.sh
@@ -3,6 +3,6 @@
 # get source for docker-proxy
 #
 
-DOWNLOAD="https://github.com/moby/libnetwork/archive/1f3b98b/libnetwork-1f3b98be6833a93f254aa0f765ff55d407dfdd69.tar.gz"
+DOWNLOAD="https://github.com/moby/libnetwork/archive/05b93e0/libnetwork-05b93e0d3a95952f70c113b0bc5bdb538d7afdd7.tar.gz"
 
 wget -c $DOWNLOAD
