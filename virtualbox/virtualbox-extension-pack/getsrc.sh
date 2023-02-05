90bed27d9ec38314e9fec7a0886fa41a951b539b
Francisco Ambrozio <dev@franciscoambrozio.com>
2023-02-04 21:56:16 -0300

    virtualbox: Updated for version 6.1.42

--- virtualbox/virtualbox-extension-pack/getsrc.sh
+++ virtualbox/virtualbox-extension-pack/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for virtualbox-extension-pack
 #
 
-VERSION="6.1.40"
+VERSION="6.1.42"
 DOWNLOAD="http://download.virtualbox.org/virtualbox/$VERSION/Oracle_VM_VirtualBox_Extension_Pack-$VERSION.vbox-extpack"
 
 wget -c $DOWNLOAD
