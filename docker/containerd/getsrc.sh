a374f82419176c9532149ec7e9647e6bc777f533
Francisco Ambrozio <dev@franciscoambrozio.com>
2023-03-06 09:48:54 -0300

    containerd: Updated for version 1.6.19 docker-cli: Updated for version 23.0.1 docker: Updated for version 23.0.1

--- docker/containerd/getsrc.sh
+++ docker/containerd/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for containerd
 #
 
-VERSION="1.6.16"
+VERSION="1.6.19"
 DOWNLOAD="https://github.com/containerd/containerd/archive/v$VERSION/containerd-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
--- docker/docker/getsrc.sh
+++ docker/docker/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for docker
 #
 
-VERSION="23.0.0"
+VERSION="23.0.1"
 DOWNLOAD="https://github.com/moby/moby/archive/v$VERSION/moby-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
