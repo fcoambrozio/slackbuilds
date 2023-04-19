9f973301cc38e3e0241116d46eeac486e3e5c1f5
Francisco Ambrozio <dev@franciscoambrozio.com>
2023-04-19 17:01:00 -0300

    Update docker to version 23.0.4 and remove docker-proxy

--- docker/docker-cli/getsrc.sh
+++ docker/docker-cli/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for docker-cli
 #
 
-VERSION="23.0.2"
+VERSION="23.0.4"
 DOWNLOAD="https://github.com/docker/cli/archive/v$VERSION/cli-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
--- docker/docker-proxy/getsrc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# get source for docker-proxy
-#
-
-DOWNLOAD="https://github.com/moby/libnetwork/archive/05b93e0/libnetwork-05b93e0d3a95952f70c113b0bc5bdb538d7afdd7.tar.gz"
-
-wget -c $DOWNLOAD
