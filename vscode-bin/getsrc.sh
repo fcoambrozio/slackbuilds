47f717d3fcbe434c62f1f846400c6815c7fe2094
Francisco Ambrozio <dev@franciscoambrozio.com>
2023-04-19 16:47:35 -0300

    vscode-bin: Updated for version 1.77.3

--- vscode-bin/getsrc.sh
+++ vscode-bin/getsrc.sh
@@ -3,6 +3,6 @@
 # get source for vscode-bin
 #
 
-DOWNLOAD="https://az764295.vo.msecnd.net/stable/ee2b180d582a7f601fa6ecfdad8d9fd269ab1884/code-stable-x64-1678818101.tar.gz"
+DOWNLOAD="https://az764295.vo.msecnd.net/stable/704ed70d4fd1c6bd6342c436f1ede30d1cff4710/code-stable-x64-1681293081.tar.gz"
 
 wget -c $DOWNLOAD
