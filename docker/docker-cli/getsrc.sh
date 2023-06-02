09bec2cc3c8d4853afe30fcb030bbf5ef9fd292d
Francisco Ambrozio <dev@franciscoambrozio.com>
2023-06-02 08:40:51 -0300

    docker: Updated for version 24.0.2

--- docker/docker-cli/getsrc.sh
+++ docker/docker-cli/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for docker-cli
 #
 
-VERSION="23.0.6"
+VERSION="24.0.2"
 DOWNLOAD="https://github.com/docker/cli/archive/v$VERSION/cli-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
--- docker/docker/getsrc.sh
+++ docker/docker/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for docker
 #
 
-VERSION="23.0.6"
+VERSION="24.0.2"
 DOWNLOAD="https://github.com/moby/moby/archive/v$VERSION/moby-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
