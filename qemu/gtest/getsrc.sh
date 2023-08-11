126a107a1c651ff94008433a5424d39f883abee0
Francisco Ambrozio <dev@franciscoambrozio.com>
2023-08-11 09:30:06 -0300

    gtest: Updated for version 1.14.0

--- qemu/gtest/getsrc.sh
+++ qemu/gtest/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for gtest - https://github.com/google/googletest
 #
 
-VERSION="1.13.0"
-DOWNLOAD="https://github.com/google/googletest/archive/refs/tags/v1.13.0.tar.gz"
+VERSION="1.14.0"
+DOWNLOAD="https://github.com/google/googletest/archive/refs/tags/v$VERSION.tar.gz"
 
 wget -c $DOWNLOAD -O googletest-$VERSION.tar.gz
