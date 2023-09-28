45774925672e25f0ee239295d0c203567d806ddc
Francisco Ambrozio <dev@franciscoambrozio.com>
2023-09-28 09:35:35 -0300

    qemu: Updated for version 8.1.1 virglrenderer: Updated for version 1.0.0 benchmark: Updated for version 1.8.3 gperftools: Updated for version 2.13 gtest, libnfs, snappy, vde2, wolfssl: Rebuilt

--- qemu/benchmark/getsrc.sh
+++ qemu/benchmark/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for benchmark - https://github.com/google/benchmark
 #
 
-VERSION="1.8.2"
+VERSION="1.8.3"
 DOWNLOAD="https://github.com/google/benchmark/archive/v$VERSION/benchmark-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
--- qemu/virglrenderer/getsrc.sh
+++ qemu/virglrenderer/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for virglrenderer - https://docs.mesa3d.org/drivers/virgl
 #
 
-VERSION="0.10.4"
+VERSION="1.0.0"
 DOWNLOAD="https://gitlab.freedesktop.org/virgl/virglrenderer/-/archive/$VERSION/virglrenderer-$VERSION.tar.bz2"
 
 wget -c $DOWNLOAD
