bb5b7952ca7cb1256fc8ccacde696cc5cecf4351
Francisco Ambrozio <dev@franciscoambrozio.com>
2023-10-11 07:28:11 -0300

    containerd: Updated for version 1.7.6

--- docker/containerd/getsrc.sh
+++ docker/containerd/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for containerd
 #
 
-VERSION="1.7.5"
+VERSION="1.7.6"
 DOWNLOAD="https://github.com/containerd/containerd/archive/v$VERSION/containerd-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
