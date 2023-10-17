ec50b81241911001aed2339e87662de2cf5dbed6
Francisco Ambrozio <dev@franciscoambrozio.com>
2023-10-17 10:01:51 -0300

    google-go-lang: Updated for version 1.21.3

--- google-go-lang/getsrc.sh
+++ google-go-lang/getsrc.sh
@@ -3,8 +3,8 @@
 # get source for google-go-lang
 #
 
-VERSION="1.21.1"
-BOOTSTRAP_VERSION="1.19.13"
+VERSION="1.21.3"
+BOOTSTRAP_VERSION="1.20.10"
 DOWNLOAD="https://go.dev/dl/go$VERSION.src.tar.gz \
           https://go.dev/dl/go$BOOTSTRAP_VERSION.src.tar.gz"
 
