c0d8c7ea352818c5d6649762e8b209b459232ac1
Francisco Ambrozio <dev@franciscoambrozio.com>
2023-10-26 08:56:53 -0300

    virtualbox-extension-pack: Updated for version 7.0.12

--- virtualbox/virtualbox-extension-pack/getsrc.sh
+++ virtualbox/virtualbox-extension-pack/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for virtualbox-extension-pack
 #
 
-VERSION="7.0.10"
+VERSION="7.0.12"
 DOWNLOAD="http://download.virtualbox.org/virtualbox/$VERSION/Oracle_VM_VirtualBox_Extension_Pack-$VERSION.vbox-extpack"
 
 wget -c $DOWNLOAD
