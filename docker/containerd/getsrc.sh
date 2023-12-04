81ee62684ec3a48c7625e50634bca9bd44ed5e23
Francisco Ambrozio <dev@franciscoambrozio.com>
2023-12-04 11:04:00 -0300

    December 2023 update: docker

--- docker/containerd/getsrc.sh
+++ docker/containerd/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for containerd
 #
 
-VERSION="1.7.8"
+VERSION="1.7.10"
 DOWNLOAD="https://github.com/containerd/containerd/archive/v$VERSION/containerd-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
