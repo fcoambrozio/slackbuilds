a3bfb6148c3f41d689e3a5ab115d58bc28d53d87
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-01-01 20:50:11 -0300

    containerd: Updated for version 1.7.11

--- docker/containerd/getsrc.sh
+++ docker/containerd/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for containerd
 #
 
-VERSION="1.7.10"
+VERSION="1.7.11"
 DOWNLOAD="https://github.com/containerd/containerd/archive/v$VERSION/containerd-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
