e97de07a81feafc010d67cfe466e890007c2f4e0
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-04-08 10:06:52 -0300

    snappy: Updated for version 1.2.0

--- qemu/snappy/getsrc.sh
+++ qemu/snappy/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for snappy - https://github.com/google/snappy
 #
 
-VERSION="1.1.10"
+VERSION="1.2.0"
 DOWNLOAD="https://github.com/google/snappy/archive/$VERSION/snappy-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
