4b7c96cfb02d3a10136753c5688607dc660581b5
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-04-08 09:09:11 -0300

    google-go-lang: Updated for version 1.22.2 noto-emoji: Updated for version 2.042

--- noto-emoji/getsrc.sh
+++ noto-emoji/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for noto-emoji
 #
 
-VERSION="2.038"
+VERSION="2.042"
 DOWNLOAD="https://github.com/googlei18n/noto-emoji/archive/v$VERSION/noto-emoji-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
