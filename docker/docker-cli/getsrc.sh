4ea089a7c2719017ae2c2f3a9a697751c6d0de0c
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-04-18 09:23:49 -0300

    docker{,-cli}: Updated for version 26.0.1

--- docker/docker-cli/getsrc.sh
+++ docker/docker-cli/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for docker-cli
 #
 
-VERSION="26.0.0"
+VERSION="26.0.1"
 DOWNLOAD="https://github.com/docker/cli/archive/v$VERSION/cli-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
--- docker/docker/getsrc.sh
+++ docker/docker/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for docker
 #
 
-VERSION="26.0.0"
+VERSION="26.0.1"
 DOWNLOAD="https://github.com/moby/moby/archive/v$VERSION/moby-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
