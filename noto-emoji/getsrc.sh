7e94bae7d6ddd78ff2f04d696a56247ca2e86532
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-09-09 09:06:21 -0300

    noto-emoji: Removed, since it was added to Slackware tree

--- noto-emoji/getsrc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# get source for noto-emoji
-#
-
-VERSION="2.042"
-DOWNLOAD="https://github.com/googlei18n/noto-emoji/archive/v$VERSION/noto-emoji-$VERSION.tar.gz"
-
-wget -c $DOWNLOAD
