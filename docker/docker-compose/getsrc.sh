d466c82c27b60ebff3f4b81bc7c5dabde0053eba
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-09-12 16:03:29 -0300

    containerd: Updated for version 1.7.22 docker-buildx: Updated for version 0.17.0 docker-cli: Updated for version 27.2.1 docker-compose: Updated for version 2.29.3 docker: Updated for version 27.2.1

--- docker/docker-compose/getsrc.sh
+++ docker/docker-compose/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for docker-compose
 #
 
-VERSION="2.29.2"
+VERSION="2.29.3"
 DOWNLOAD="https://github.com/docker/compose/archive/v$VERSION/compose-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
--- docker/docker/getsrc.sh
+++ docker/docker/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for docker
 #
 
-VERSION="27.2.0"
+VERSION="27.2.1"
 DOWNLOAD="https://github.com/moby/moby/archive/v$VERSION/moby-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
