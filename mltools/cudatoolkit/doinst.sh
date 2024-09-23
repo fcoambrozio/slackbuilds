22d3e81bc48df92f04b8757151b0d15c65fa20bb
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-09-23 18:21:42 -0300

    cudatoolkit: Improved profile file

--- mltools/cudatoolkit/doinst.sh
+++ mltools/cudatoolkit/doinst.sh
@@ -1,3 +1,25 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+# Keep same perms on cuda.{,}sh.new:
+if [ -e etc/profile.d/cuda.sh ]; then
+  cp -a etc/profile.d/cuda.sh etc/profile.d/cuda.sh.incoming
+  cat etc/profile.d/cuda.sh.new > etc/profile.d/cuda.sh.incoming
+  mv etc/profile.d/cuda.sh.incoming etc/profile.d/cuda.sh.new
+fi
+
+config etc/profile.d/cuda.sh.new
+
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   if [ -x /usr/bin/gtk-update-icon-cache ]; then
     /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
--- mltools/cudatoolkit/files/cuda.sh
+++ mltools/cudatoolkit/files/cuda.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 export CUDA_PATH=/opt/cuda
-export PATH=${PATH}:/opt/cuda/bin:/opt/cuda/nsight_compute:/opt/cuda/nsight_systems/bin
-export LD_LIBRARY_PATH=/opt/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 export NVCC_CCBIN=/usr/bin/g++-13
+export PATH=${PATH}:/opt/cuda/bin:/opt/cuda/nsight_compute:/opt/cuda/nsight_systems/bin
+
+if ! echo $LD_LIBRARY_PATH | grep -q "/opt/cuda/lib64" ; then
+    export LD_LIBRARY_PATH=/opt/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+fi
