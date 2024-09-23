6a4cd8ca9dbcd709f43d7a67a4f285aac8ea8370
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-09-23 18:17:27 -0300

    gst-plugins-{bad,ugly}: Updated for version 1.24.8

--- multimedia/gst-plugins-bad/getsrc.sh
+++ multimedia/gst-plugins-bad/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for gst-plugins-bad
 #
 
-VERSION="1.24.7"
+VERSION="1.24.8"
 DOWNLOAD="https://gstreamer.freedesktop.org/src/gst-plugins-bad/gst-plugins-bad-$VERSION.tar.xz"
 
 wget -c $DOWNLOAD
