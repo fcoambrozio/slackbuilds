36c6533bd54df26170d2bfdb0efdab4abdbb1b51
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-10-04 08:15:27 -0300

    Updated cudatoolkit to version 12.6.2

--- mltools/cudatoolkit/getsrc.sh
+++ mltools/cudatoolkit/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for cudatoolkit
 #
 
-VERSION="12.6.1"
+VERSION="12.6.2"
 DRVVER="560.35.03"
 DOWNLOAD="https://developer.download.nvidia.com/compute/cuda/$VERSION/local_installers/cuda_${VERSION}_${DRVVER}_linux.run"
 
--- mltools/nvidia-container-toolkit/nvidia-container-toolkit/getsrc.sh
+++ mltools/nvidia-container-toolkit/nvidia-container-toolkit/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for libnvidia-container
 #
 
-VERSION="1.16.1"
+VERSION="1.16.2"
 DOWNLOAD="https://github.com/NVIDIA/nvidia-container-toolkit/archive/v${VERSION}/nvidia-container-toolkit-${VERSION}.tar.gz"
 
 wget -c $DOWNLOAD
