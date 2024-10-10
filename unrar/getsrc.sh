d3d574f90abf248ae2eaabc23fec4ecd99578e12
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-10-10 10:06:29 -0300

    Added unrar

---
+++ unrar/getsrc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# get source for unrar - https://www.rarlab.com/rar_add.htm
+#
+
+VERSION="7.0.9"
+DOWNLOAD="https://www.rarlab.com/rar/unrarsrc-$VERSION.tar.gz"
+
+wget -c $DOWNLOAD
