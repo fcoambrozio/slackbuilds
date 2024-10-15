94f1a339a4bf111f99b78b3f5381683a6a0da9a7
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-10-15 18:12:22 -0300

    cudnn: Updated for version 9.5.0.50

--- mltools/cudnn/getsrc.sh
+++ mltools/cudnn/getsrc.sh
@@ -4,7 +4,7 @@
 #
 
 CUDAMJVER=12
-VERSION=9.4.0.58
+VERSION=9.5.0.50
 DOWNLOAD="https://developer.download.nvidia.com/compute/cudnn/redist/cudnn/linux-x86_64/cudnn-linux-x86_64-${VERSION}_cuda${CUDAMJVER}-archive.tar.xz"
 
 wget -c $DOWNLOAD
