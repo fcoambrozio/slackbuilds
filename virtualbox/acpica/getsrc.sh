288403da8642f476c336d21e9d5b8b16daf9d4db
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-11-11 11:19:27 -0300

    acpica: Updated for version 20240927

--- virtualbox/acpica/getsrc.sh
+++ virtualbox/acpica/getsrc.sh
@@ -3,6 +3,6 @@
 # get source for acpica - http://www.acpica.org
 #
 
-DOWNLOAD="https://github.com/user-attachments/files/16769900/acpica-unix-20240827.tar.gz"
+DOWNLOAD="https://github.com/user-attachments/files/17171019/acpica-unix-20240927.tar.gz"
 
 wget -c $DOWNLOAD
