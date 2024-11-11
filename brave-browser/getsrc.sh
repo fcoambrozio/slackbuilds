d7b2c841f0ef849efaaceea0a6ec5e59691fe278
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-11-11 11:21:31 -0300

    brave-browser: Added

---
+++ brave-browser/getsrc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# get source for brave-browser
+#
+
+VERSION="1.71.123"
+DOWNLOAD="https://github.com/brave/brave-browser/releases/download/v${VERSION}/brave-browser_${VERSION}_amd64.deb"
+
+wget -c $DOWNLOAD
