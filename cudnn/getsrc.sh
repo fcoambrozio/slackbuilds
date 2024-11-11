adadc2965902a742e4516892af495abcf64ebc1d
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-11-11 11:42:28 -0300

    Organizing ml-tools builds scripts

--- cudnn/getsrc.sh
+++ cudnn/getsrc.sh
@@ -4,7 +4,7 @@
 #
 
 CUDAMJVER=12
-VERSION=9.5.0.50
+VERSION=9.5.1.17
 DOWNLOAD="https://developer.download.nvidia.com/compute/cudnn/redist/cudnn/linux-x86_64/cudnn-linux-x86_64-${VERSION}_cuda${CUDAMJVER}-archive.tar.xz"
 
 wget -c $DOWNLOAD
