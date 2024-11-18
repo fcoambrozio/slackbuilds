0b6735b393ffbdac66822d94017ed80b729822c4
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-11-18 09:04:35 -0300

    device-tree-compiler: Updated for version 1.7.2

--- qemu/device-tree-compiler/getsrc.sh
+++ qemu/device-tree-compiler/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for device-tree-compiler
 #
 
-VERSION="1.7.1"
+VERSION="1.7.2"
 DOWNLOAD="https://mirrors.edge.kernel.org/pub/software/utils/dtc/dtc-$VERSION.tar.xz"
 
 wget -c $DOWNLOAD
