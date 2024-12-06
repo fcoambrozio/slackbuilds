feb9bb481a8290f8ca27c8e5fa3f3322a46eb861
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-12-06 11:22:59 -0300

    nvidia-driver: Updated for version 565.77

--- nvidia-driver/nvidia-kernel/getsrc.sh
+++ nvidia-driver/nvidia-kernel/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for nvidia-kernel
 #
 
-VERSION="550.135"
+VERSION="565.77"
 DOWNLOAD="https://download.nvidia.com/XFree86/Linux-x86_64/$VERSION/NVIDIA-Linux-x86_64-$VERSION.run"
 
 wget -c $DOWNLOAD
