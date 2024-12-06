64f44ecf6c9b6486aa45b19c944fd5774edb78b1
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-12-06 11:22:22 -0300

    brave-browser: Updated for version 1.73.97

--- brave-browser/getsrc.sh
+++ brave-browser/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for brave-browser
 #
 
-VERSION="1.73.91"
+VERSION="1.73.97"
 DOWNLOAD="https://github.com/brave/brave-browser/releases/download/v${VERSION}/brave-browser_${VERSION}_amd64.deb"
 
 wget -c $DOWNLOAD
