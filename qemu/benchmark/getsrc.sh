384b35545c9b77ee756e792ab804cf3e94a568a8
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-12-06 09:56:32 -0300

    benchmark: Updated for version 1.9.1

--- qemu/benchmark/getsrc.sh
+++ qemu/benchmark/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for benchmark - https://github.com/google/benchmark
 #
 
-VERSION="1.9.0"
+VERSION="1.9.1"
 DOWNLOAD="https://github.com/google/benchmark/archive/v$VERSION/benchmark-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
