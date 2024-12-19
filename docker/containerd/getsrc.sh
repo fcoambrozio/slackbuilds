497a49c4fef5abfe2b1a173caea61b3361093f36
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-12-18 23:00:41 -0300

    Some updates for docker stack

--- docker/containerd/getsrc.sh
+++ docker/containerd/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for containerd
 #
 
-VERSION="2.0.0"
+VERSION="2.0.1"
 DOWNLOAD="https://github.com/containerd/containerd/archive/v$VERSION/containerd-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
--- docker/docker-compose/getsrc.sh
+++ docker/docker-compose/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for docker-compose
 #
 
-VERSION="2.31.0"
+VERSION="2.32.1"
 DOWNLOAD="https://github.com/docker/compose/archive/v$VERSION/compose-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
