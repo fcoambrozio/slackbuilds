657c16a88a58261fd0509cd4eecf39f6b6cdec86
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-12-27 10:12:42 -0300

    flatpak clean up

--- flatpak/xdg-desktop-portal/getsrc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# get source for xdg-dbus-proxy
-#
-
-VERSION="0.1.6"
-DOWNLOAD="https://github.com/flatpak/xdg-dbus-proxy/releases/download/${VERSION}/xdg-dbus-proxy-${VERSION}.tar.xz"
-
-wget -c $DOWNLOAD
