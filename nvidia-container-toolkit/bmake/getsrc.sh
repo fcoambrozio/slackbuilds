e233f1db9d440b1be322da438fce172717bfe7e7
Francisco Ambrozio <dev@franciscoambrozio.com>
2024-12-27 10:11:05 -0300

    nvidia-container-toolkit: Updated for version 1.17.3

--- nvidia-container-toolkit/bmake/getsrc.sh
+++ nvidia-container-toolkit/bmake/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for bmake
 #
 
-VERSION="20240921"
+VERSION="20241124"
 DOWNLOAD="https://www.crufty.net/ftp/pub/sjg/bmake-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
--- nvidia-container-toolkit/nvidia-container-toolkit/getsrc.sh
+++ nvidia-container-toolkit/nvidia-container-toolkit/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for libnvidia-container
 #
 
-VERSION="1.17.2"
+VERSION="1.17.3"
 DOWNLOAD="https://github.com/NVIDIA/nvidia-container-toolkit/archive/v${VERSION}/nvidia-container-toolkit-${VERSION}.tar.gz"
 
 wget -c $DOWNLOAD
