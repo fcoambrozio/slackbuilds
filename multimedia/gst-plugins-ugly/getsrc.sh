dc14e3a1c6ad00aa8fef0ec110ac5847fe57472a
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-01-21 08:35:58 -0300

    2025 Jan updates

--- multimedia/gst-plugins-ugly/getsrc.sh
+++ multimedia/gst-plugins-ugly/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for gst-plugins-ugly - https://gstreamer.freedesktop.org/
 #
 
-VERSION="1.24.10"
+VERSION="1.24.11"
 DOWNLOAD="https://gstreamer.freedesktop.org/src/gst-plugins-ugly/gst-plugins-ugly-$VERSION.tar.xz"
 
 wget -c $DOWNLOAD
