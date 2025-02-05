0502eedf8bfd02a42ad9fa645af075ee598a3650
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-02-05 15:24:38 -0300

    brave-browser: Updated for version 1.74.51 gst-plugins-bad: Updated for version 1.24.12 gst-plugins-ugly: Updated for version 1.24.12 virtualbox-kernel: Recompiled against kernel 6.12.12

--- brave-browser/getsrc.sh
+++ brave-browser/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for brave-browser
 #
 
-VERSION="1.74.50"
+VERSION="1.74.51"
 DOWNLOAD="https://github.com/brave/brave-browser/releases/download/v${VERSION}/brave-browser_${VERSION}_amd64.deb"
 
 wget -c $DOWNLOAD
--- multimedia/gst-plugins-ugly/getsrc.sh
+++ multimedia/gst-plugins-ugly/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for gst-plugins-ugly - https://gstreamer.freedesktop.org/
 #
 
-VERSION="1.24.11"
+VERSION="1.24.12"
 DOWNLOAD="https://gstreamer.freedesktop.org/src/gst-plugins-ugly/gst-plugins-ugly-$VERSION.tar.xz"
 
 wget -c $DOWNLOAD
