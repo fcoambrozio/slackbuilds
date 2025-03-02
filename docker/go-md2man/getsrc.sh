160eba8b06704db564168dd99e7009540090032a
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-03-02 09:09:25 -0300

    go-md2man: Added

---
+++ docker/go-md2man/getsrc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# get sources for go-md2man - https://github.com/cpuguy83/go-md2man
+#
+
+
+VERSION="2.0.6"
+DOWNLOAD="https://github.com/cpuguy83/go-md2man/archive/v${VERSION}/go-md2man-${VERSION}.tar.gz"
+
+wget -c $DOWNLOAD
