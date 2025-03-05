1035141f6724dd4c02ae49cc96c0f2f8f29978e8
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-03-05 11:25:00 -0300

    faac: Updated for version 1.31.1 faad2: Updated for version 2.11.2

--- multimedia/faac/getsrc.sh
+++ multimedia/faac/getsrc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #
-# get source for faac
+# get source for faac - https://github.com/knik0/faac
 #
 
-VERSION="1.31"
+
+VERSION="1.31.1"
 DOWNLOAD="https://github.com/knik0/faac/archive/refs/tags/faac-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
--- multimedia/faad2/getsrc.sh
+++ multimedia/faad2/getsrc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #
-# get source for faad2
+# get source for faad2 - https://github.com/knik0/faad2
 #
 
-VERSION="2.11.1"
+
+VERSION="2.11.2"
 DOWNLOAD="https://github.com/knik0/faad2/archive/$VERSION/faad2-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
