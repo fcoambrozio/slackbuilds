c91b112540abaf64749e8feba7a32d87be8e12b1
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-03-06 12:23:07 -0300

    TLP: Added

---
+++ TLP/getsrc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# get source for TLP - https://github.com/linrunner/TLP
+#
+
+
+VERSION="1.8.0"
+DOWNLOAD="https://github.com/linrunner/TLP/archive/${VERSION}/TLP-${VERSION}.tar.gz"
+
+wget -c $DOWNLOAD
