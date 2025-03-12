0f75d523e404cf5e05d7fd5bcf30c25ea6134da9
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-03-12 09:02:40 -0300

    qemu: Updated for version 9.2.2 gperftools: Updated for version 2.16.90

--- qemu/gperftools/getsrc.sh
+++ qemu/gperftools/getsrc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #
-# get source for gperftools
+# get source for gperftools - https://github.com/gperftools/gperftools
 #
 
-VERSION="2.16"
+
+VERSION="2.16.90"
 DOWNLOAD="https://github.com/gperftools/gperftools/archive/gperftools-$VERSION/gperftools-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
--- qemu/qemu/getsrc.sh
+++ qemu/qemu/getsrc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #
-# get source for qemu
+# get source for qemu - https://www.qemu.org
 #
 
-VERSION="9.2.1"
+
+VERSION="9.2.2"
 DOWNLOAD="https://download.qemu.org/qemu-$VERSION.tar.xz"
 
 wget -c $DOWNLOAD
