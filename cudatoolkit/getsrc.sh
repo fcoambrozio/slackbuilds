d8b8bde33e584a25270fb9201a95969eecb06dde
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-03-14 11:42:37 -0300

    cudatoolkit: Updated for version 12.8.1

--- cudatoolkit/getsrc.sh
+++ cudatoolkit/getsrc.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #
-# get source for cudatoolkit
+# get source for cudatoolkit - https://developer.nvidia.com/cuda-downloads
 #
 
-VERSION="12.8.0"
-DRVVER="570.86.10"
+
+VERSION="12.8.1"
+DRVVER="570.124.06"
 DOWNLOAD="https://developer.download.nvidia.com/compute/cuda/$VERSION/local_installers/cuda_${VERSION}_${DRVVER}_linux.run"
 
 wget -c $DOWNLOAD
