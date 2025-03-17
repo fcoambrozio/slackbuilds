43a1763e47a0099e4afaedea231b6004a76c0893
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-03-17 11:50:03 -0300

    Mass refactoring - numactl

--- numactl/getsrc.sh
+++ numactl/getsrc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
-# get source for numactl
+# get source for numactl - https://github.com/numactl/numactl
 #
+
 
 VERSION="2.0.19"
 DOWNLOAD="https://github.com/numactl/numactl/releases/download/v$VERSION/numactl-$VERSION.tar.gz"
