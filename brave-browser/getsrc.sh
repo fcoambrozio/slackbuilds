f5b0038ab72e7a8647c9ff6fafec6cbee80a2863
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-03-19 11:35:28 -0300

    Removed distrobox and brave-browser

--- brave-browser/getsrc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# get source for brave-browser
-#
-
-VERSION="1.75.175"
-DOWNLOAD="https://github.com/brave/brave-browser/releases/download/v${VERSION}/brave-browser_${VERSION}_amd64.deb"
-
-wget -c $DOWNLOAD
--- distrobox/getsrc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# get source for distrobox
-#
-
-VERSION="1.6.0.1"
-DOWNLOAD="https://github.com/89luca89/distrobox/archive/refs/tags/$VERSION.tar.gz"
-
-wget -c $DOWNLOAD -O distrobox-$VERSION.tar.gz
