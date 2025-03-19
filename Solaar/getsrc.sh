9fbc4e8e38eedb3890c8252d1c89c61cb3c4ea98
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-03-19 11:44:59 -0300

    Solaar: Added

---
+++ Solaar/getsrc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# get source for Solaar - https://github.com/pwr-Solaar/Solaar
+#
+
+
+VERSION="1.1.14"
+DOWNLOAD="https://github.com/pwr-Solaar/Solaar/archive/${VERSION}/Solaar-${VERSION}.tar.gz"
+
+wget -c $DOWNLOAD
