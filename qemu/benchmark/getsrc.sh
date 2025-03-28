3d42f294f32365761cf398b58cb839f81733a17d
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-03-28 14:58:58 -0300

    benchmark: Updated for 1.9.2, but it seems that snappy doesn't depends on this anymore

--- qemu/benchmark/getsrc.sh
+++ qemu/benchmark/getsrc.sh
@@ -3,7 +3,8 @@
 # get source for benchmark - https://github.com/google/benchmark
 #
 
-VERSION="1.9.1"
+
+VERSION="1.9.2"
 DOWNLOAD="https://github.com/google/benchmark/archive/v$VERSION/benchmark-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
