d243d2b8b73879342941771f32202ca9c028474d
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-05-05 08:48:02 -0300

    Updated nvidia-driver for version 575.51.02

--- nvidia-driver/nvidia-driver/getsrc.sh
+++ nvidia-driver/nvidia-driver/getsrc.sh
@@ -3,8 +3,7 @@
 # get source for nvidia-driver - https://www.nvidia.com/en-us/drivers/unix/
 #
 
-
-VERSION="570.144"
+VERSION="575.51.02"
 DOWNLOAD="https://download.nvidia.com/XFree86/Linux-x86_64/$VERSION/NVIDIA-Linux-x86_64-$VERSION.run \
           https://download.nvidia.com/XFree86/nvidia-installer/nvidia-installer-$VERSION.tar.bz2 \
           https://download.nvidia.com/XFree86/nvidia-modprobe/nvidia-modprobe-$VERSION.tar.bz2 \
--- nvidia-driver/nvidia-kernel/getsrc.sh
+++ nvidia-driver/nvidia-kernel/getsrc.sh
@@ -3,8 +3,7 @@
 # get source for nvidia-kernel - https://www.nvidia.com/en-us/drivers/unix/
 #
 
-
-VERSION="570.144"
+VERSION="575.51.02"
 DOWNLOAD="https://download.nvidia.com/XFree86/Linux-x86_64/$VERSION/NVIDIA-Linux-x86_64-$VERSION.run"
 
 wget -c $DOWNLOAD
