5c84e4783a52fe0f67ebebe41d11a06925b97e5c
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-05-13 10:23:26 -0300

    containerd: Updated for version 2.1.0 docker-compose: Updated for version 2.36.0 runc: Updated for version 1.3.0

--- docker/containerd/getsrc.sh
+++ docker/containerd/getsrc.sh
@@ -3,8 +3,7 @@
 # get sources for containerd - https://github.com/containerd/containerd
 #
 
-
-VERSION="2.0.5"
+VERSION="2.1.0"
 DOWNLOAD="https://github.com/containerd/containerd/archive/v$VERSION/containerd-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
