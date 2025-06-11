9e0a0f4af4aa650354c22641cf4ecb45c1acf437
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-06-11 09:20:01 -0300

    nvidia-driver: Updated for version 575.57.08

--- nvidia-driver/nvidia-kernel/getsrc.sh
+++ nvidia-driver/nvidia-kernel/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for nvidia-kernel - https://www.nvidia.com/en-us/drivers/unix/
 #
 
-VERSION="575.51.02"
+VERSION="575.57.08"
 DOWNLOAD="https://download.nvidia.com/XFree86/Linux-x86_64/$VERSION/NVIDIA-Linux-x86_64-$VERSION.run"
 
 wget -c $DOWNLOAD
