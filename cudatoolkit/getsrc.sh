c9973aeb93aff4d6f6c76c46f76cdce6fb004514
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-06-18 09:59:31 -0300

    cudnn: Updated for version 9.10.2.21 cudatoolkit: Updated for version 12.9.1

--- cudatoolkit/getsrc.sh
+++ cudatoolkit/getsrc.sh
@@ -3,8 +3,8 @@
 # get source for cudatoolkit - https://developer.nvidia.com/cuda-downloads
 #
 
-VERSION="12.9.0"
-DRVVER="575.51.03"
+VERSION="12.9.1"
+DRVVER="575.57.08"
 DOWNLOAD="https://developer.download.nvidia.com/compute/cuda/$VERSION/local_installers/cuda_${VERSION}_${DRVVER}_linux.run"
 
 wget -c $DOWNLOAD
--- cudnn/getsrc.sh
+++ cudnn/getsrc.sh
@@ -4,7 +4,7 @@
 #
 
 CUDAVER=12
-VERSION=9.9.0.52
+VERSION=9.10.2.21
 DOWNLOAD="https://developer.download.nvidia.com/compute/cudnn/redist/cudnn/linux-x86_64/cudnn-linux-x86_64-${VERSION}_cuda${CUDAVER}-archive.tar.xz"
 
 wget -c $DOWNLOAD
