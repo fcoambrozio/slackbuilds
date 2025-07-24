02c10e45d0c51f03b0c0eba9901361f2434d8678
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-07-24 08:22:28 -0300

    Updates for gst-plugins, nvidia-driver, ollama, wolfssl, spotify and vscode

--- qemu/wolfssl/getsrc.sh
+++ qemu/wolfssl/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for wolfssl
 #
 
-VERSION="5.8.0"
+VERSION="5.8.2"
 DOWNLOAD="https://github.com/wolfSSL/wolfssl/archive/refs/tags/v$VERSION-stable.tar.gz"
 
 wget -c $DOWNLOAD -O wolfssl-$VERSION-stable.tar.gz
--- spotify/spotify/getsrc.sh
+++ spotify/spotify/getsrc.sh
@@ -9,9 +9,8 @@ case "$( uname -m )" in
        *) ARCH=$( uname -m ) ;;
 esac
 
-
 if [ "$ARCH" = "x86_64" ]; then
-    DOWNLOAD="https://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.2.60.564.gcc6305cb_amd64.deb"
+    DOWNLOAD="https://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.2.63.394.g126b0d89_amd64.deb"
 else
     DOWNLOAD="http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.0.72.117.g6bd7cc73-35_i386.deb"
 fi
