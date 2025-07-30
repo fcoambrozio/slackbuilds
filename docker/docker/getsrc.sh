39786675c5ed00a87bd47cf8bcdf1703a063ed05
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-07-30 08:49:10 -0300

    Updates on docker stack

--- docker/docker/getsrc.sh
+++ docker/docker/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for docker - https://github.com/moby/moby
 #
 
-VERSION="28.3.2"
+VERSION="28.3.3"
 DOWNLOAD="https://github.com/moby/moby/archive/v$VERSION/moby-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
