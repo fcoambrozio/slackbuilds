e259ac86fa1b84b943e04cc94446703cbc7cf88b
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-10-02 08:14:01 -0300

    Updates on nvidia-driver, ollama and a few more

--- nvidia-driver/nvidia-kernel/getsrc.sh
+++ nvidia-driver/nvidia-kernel/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for nvidia-kernel - https://www.nvidia.com/en-us/drivers/unix/
 #
 
-VERSION="580.82.09"
+VERSION="580.95.05"
 DOWNLOAD="https://us.download.nvidia.com/XFree86/Linux-x86_64/$VERSION/NVIDIA-Linux-x86_64-$VERSION.run"
 
 wget -c $DOWNLOAD
