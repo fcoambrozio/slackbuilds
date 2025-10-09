bdb59a00f5dc65bce0b04bd70759a8aba4f87887
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-10-09 10:25:40 -0300

    qemu: Updated for version 10.1.1

--- qemu/qemu/getsrc.sh
+++ qemu/qemu/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for qemu - https://www.qemu.org
 #
 
-VERSION="10.1.0"
+VERSION="10.1.1"
 DOWNLOAD="https://download.qemu.org/qemu-$VERSION.tar.xz"
 
 wget -c $DOWNLOAD
--- qemu/snappy/getsrc.sh
+++ qemu/snappy/getsrc.sh
@@ -3,7 +3,6 @@
 # get source for snappy - https://github.com/google/snappy
 #
 
-
 VERSION="1.2.2"
 DOWNLOAD="https://github.com/google/snappy/archive/$VERSION/snappy-$VERSION.tar.gz"
 
