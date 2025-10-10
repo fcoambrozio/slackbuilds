1cc163b4af5a8c4d98dd8464e2e7b209cc872fb5
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-10-10 10:10:34 -0300

    Renamed cudatoolkit to cuda-toolkit and updated ollama for version 0.12.4

--- cuda-toolkit/getsrc.sh
+++ cuda-toolkit/getsrc.sh
@@ -3,8 +3,8 @@
 # get source for cudatoolkit - https://developer.nvidia.com/cuda-downloads
 #
 
-VERSION="13.0.1"
-DRVVER="580.82.07"
+VERSION="13.0.2"
+DRVVER="580.95.05"
 DOWNLOAD="https://developer.download.nvidia.com/compute/cuda/$VERSION/local_installers/cuda_${VERSION}_${DRVVER}_linux.run"
 
 wget -c $DOWNLOAD
