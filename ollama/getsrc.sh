0fca8b9af94de078d07f2cf671c352af14ee7815
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-10-26 21:02:17 -0300

    ollama: Updated for version 0.12.6

--- ollama/getsrc.sh
+++ ollama/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for ollama - https://github.com/ollama/ollama
 #
 
-VERSION=0.12.5
+VERSION=0.12.6
 DOWNLOAD="https://github.com/ollama/ollama/releases/download/v${VERSION}/ollama-linux-amd64.tgz"
 
 wget -c $DOWNLOAD
