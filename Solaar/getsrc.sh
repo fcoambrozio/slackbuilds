f669b234df5003f28ac5adffe4237a1ad533afec
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-10-26 21:44:05 -0300

    Solaar: Updated for version 1.1.16

--- Solaar/getsrc.sh
+++ Solaar/getsrc.sh
@@ -3,8 +3,7 @@
 # get source for Solaar - https://github.com/pwr-Solaar/Solaar
 #
 
-
-VERSION="1.1.14"
+VERSION="1.1.16"
 DOWNLOAD="https://github.com/pwr-Solaar/Solaar/archive/${VERSION}/Solaar-${VERSION}.tar.gz"
 
 wget -c $DOWNLOAD
