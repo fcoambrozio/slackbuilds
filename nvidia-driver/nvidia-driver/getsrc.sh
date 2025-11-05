080eeadbde0cd171cde750b44009349761bd8f33
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-11-05 08:58:38 -0300

    nvidia-driver: Updated for version 580.105.08

--- nvidia-driver/nvidia-driver/getsrc.sh
+++ nvidia-driver/nvidia-driver/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for nvidia-driver - https://www.nvidia.com/en-us/drivers/unix/
 #
 
-VERSION="580.95.05"
+VERSION="580.105.08"
 DOWNLOAD="https://us.download.nvidia.com/XFree86/Linux-x86_64/$VERSION/NVIDIA-Linux-x86_64-$VERSION.run \
           https://download.nvidia.com/XFree86/nvidia-installer/nvidia-installer-$VERSION.tar.bz2 \
           https://download.nvidia.com/XFree86/nvidia-modprobe/nvidia-modprobe-$VERSION.tar.bz2 \
