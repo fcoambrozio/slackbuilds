6c4950f194b325b6517246959a636abb55977375
Francisco Ambrozio <dev@franciscoambrozio.com>
2025-12-04 16:14:43 -0300

    docker-compose: Updated for version 5.0.0

--- docker/docker-compose/getsrc.sh
+++ docker/docker-compose/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for docker-compose - https://github.com/docker/compose
 #
 
-VERSION="2.40.3"
+VERSION="5.0.0"
 DOWNLOAD="https://github.com/docker/compose/archive/v$VERSION/compose-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
