412c52f3aafaacb4e225b72be85fd150a4644d4e
Francisco Ambrozio <dev@franciscoambrozio.com>
2026-01-08 10:14:09 -0300

    unrar: Updated for version 7.2.3

--- unrar/getsrc.sh
+++ unrar/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for unrar - https://www.rarlab.com/rar_add.htm
 #
 
-VERSION="7.2.1"
+VERSION="7.2.3"
 DOWNLOAD="https://www.rarlab.com/rar/unrarsrc-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
