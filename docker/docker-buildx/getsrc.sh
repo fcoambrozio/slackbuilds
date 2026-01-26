5c515ae7b947b828fe8c952ae4ab3ee45e8a3085
Francisco Ambrozio <dev@franciscoambrozio.com>
2026-01-26 11:45:35 -0300

    docker-buildx: Updated for version 0.31.0

--- docker/docker-buildx/getsrc.sh
+++ docker/docker-buildx/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for docker-buildx - https://github.com/docker/buildx
 #
 
-VERSION="0.30.1"
+VERSION="0.31.0"
 DOWNLOAD="https://github.com/docker/buildx/archive/v$VERSION/buildx-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
