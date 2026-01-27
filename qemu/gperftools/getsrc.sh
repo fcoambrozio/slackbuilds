d1128d9a0277df8a0c16279736591ddfd3d95719
Francisco Ambrozio <dev@franciscoambrozio.com>
2026-01-27 09:37:14 -0300

    gperftools: Updated for version 2.18

--- qemu/gperftools/getsrc.sh
+++ qemu/gperftools/getsrc.sh
@@ -3,7 +3,7 @@
 # get source for gperftools - https://github.com/gperftools/gperftools
 #
 
-VERSION="2.17.2"
+VERSION="2.18"
 DOWNLOAD="https://github.com/gperftools/gperftools/archive/gperftools-$VERSION/gperftools-$VERSION.tar.gz"
 
 wget -c $DOWNLOAD
