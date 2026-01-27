396283e329abb4b86a53336f4034e36ecf6361e6
Francisco Ambrozio <dev@franciscoambrozio.com>
2026-01-27 09:30:59 -0300

    docker: Updated for version 29.2.0

--- docker/docker/getsrc.sh
+++ docker/docker/getsrc.sh
@@ -4,7 +4,7 @@
 #
 
 PKGNAME="docker"
-VERSION="29.1.5"
+VERSION="29.2.0"
 
 SOURCE=(
   "moby-${PKGNAME}-v${VERSION}.tar.gz|https://github.com/moby/moby/archive/refs/tags/${PKGNAME}-v${VERSION}.tar.gz"
